122db1e779ab4b8f195fa2bfa3d516f080c39f67
e1b19049 <e1b19049@st.oit.ac.jp>
2021-11-16 16:08:14 +0900

    schemaの修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE shop (
-  shop_id INT IDENTITY,
-  shop_name CHAR NOT NULL
+  shop_id INT IDENTITY,
+  shop_name CHAR NOT NULL
 );
 CREATE TABLE items (
-  item_id INT IDENTITY,
-  item_name CHAR NOT NULL,
-  price INT NOT NULL
+  item_id INT IDENTITY,
+  item_name CHAR NOT NULL,
+  price INT NOT NULL
 );
 CREATE TABLE stock (
-  stock_id INT IDENTITY,
-  shop_id INT references shop(shop_id),
-  item_id INT references items(item_id),
-  number INT NOT NULL
+  shop_id INT references shop(shop_id),
+  item_id INT references items(item_id),
+  number INT NOT NULL,
+  primary key(shop_id,item_id)
 );
