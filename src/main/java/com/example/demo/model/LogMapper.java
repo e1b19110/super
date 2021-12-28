749d2efdc55a2c450b66fe3090a6a7f6617cef44
tabata <e1b19049@st.oit.ac.jp>
2021-12-28 21:56:43 +0900

    Logクラスとマッパーの作成

---
+++ src/main/java/com/example/demo/model/LogMapper.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface LogMapper {
+  @Insert("insert into log (user_id,item_id,send_shop_id,recv_shop_id,number,msg) values (#{user_id},#{item_id},#{send_shop_id},#{recv_shop_id},#{nunber},#{msg});")
+  @Options(useGeneratedKeys = true, keyColumn = "log_id", keyProperty = "log_id")
+  void insertLog(Log log);
+
+  @Select("select * from logs;")
+  ArrayList<Log> selectAllLogs();
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -18,3 +18,12 @@ CREATE TABLE users (
   user_name CHAR NOT NULL,
   shop_id INT references shop(shop_id)
 );
+CREATE TABLE logs (
+  log_id INT IDENTITY,
+  user_id INT references users(user_id),
+  item_id INT references items(item_id),
+  send_shop_id INT references shop(shop_id),
+  recv_shop_id INT references shop(shop_id),
+  number INT NOT NULL,
+  msg CHAR
+);
